357ae6fe4f0c2dc682a598e5802dd05453a9471e
guilherme-2003 <guilherme.paulela2003@gmail.com>
2021-09-20 16:47:18 -0300

    automação terminada e nomes certos

--- scripts/conversao-jpg-png.sh
+++ scripts/conversao-jpg-png.sh
@@ -3,5 +3,6 @@
 cd ~/alura-think/imagens-livros
 for imagem in *.jpg 
 do
-	convert $imagem $imagem.png
+	imagem_sem_extensao=$(ls $imagem | awk -F. '{ print $1 }')
+	convert $imagem_sem_extensao.jpg $imagem_sem_extensao.png
 done
