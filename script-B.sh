447b0a0ada02000a02b7cadd2ae192aec40ead9c
guilherme-2003 <guilherme.paulela2003@gmail.com>
2021-09-27 14:24:38 -0300

    Scripts C e B bruto funcional

--- script-B.sh
+++ script-B.sh
@@ -2,7 +2,7 @@
 
 echo "Guest 1 is: $val"
 ./script-C.sh
-echo "Guest 1 is: $val2"
+echo "Guest 2 is: $val2"
 ./script-C.sh
 echo "In day: $num"
 ./script-C.sh
--- script-C.sh
+++ script-C.sh
@@ -1,32 +1,35 @@
 #!/bin/bash
 
 read -p "Enter name of the first guest: " val
-
+if [ -z $val ]
+then
+	echo "You need to enter some information!"
+else
 echo $val|grep "^[a-zA-Z]*$"
 
 val="$?"
-export val
-./script-B.sh
 
 if [[ $val == 0 ]]
 then
 	read -p "Enter name of the second guest: " val2
-
+	if [ -z $val2 ]
+	then
+		echo "You need to enter some information!"
+	else
   echo $val2|grep "^[a-zA-Z]*$"
 
   val2="$?"	
-	export val2
-	./script-B.sh
 
   if [[ $val2 == 0 ]]
 	then
 		read -p "Enter the day: " num
-		
+		if [ -z $num ]
+		then
+				echo "You need to enter some information!"
+		else	
 		echo "$num"|grep "^[0-9]*$" 
 
 		num="$?"
-		export num
-		./script-B.sh
 
 		if [[ $num == 0 ]]
 		then
@@ -34,9 +37,12 @@ then
 		else
 			echo "Error! Invalid day!"
 
+fi
+	fi
+		fi
 		exit
 		fi
- 	exit
+	exit
 	fi
 exit
 fi
