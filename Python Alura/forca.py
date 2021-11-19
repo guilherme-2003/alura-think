ee46e639ed0255287f416208eeec437afd4723c2
guilherme-2003 <guilherme.paulela2003@gmail.com>
2021-11-19 16:56:06 -0300

    new changes

--- Python Alura/forca.py
+++ Python Alura/forca.py
@@ -4,5 +4,43 @@ def jogo():
     print('Bem vindo ao jogo da forca')
     print('*' * 26)
 
+    palavra_secreta = 'laranja'.upper()
+    letras_acertadas = ['-' for _ in palavra_secreta]
+
+    enforcou = False
+    acertou = False
+    erros = 0
+    tamanho_da_palavra = len(palavra_secreta)
+    
+    print( '_' * 4)
+    print('|  |')
+    print('|  O')
+    print('| /|\\')
+    print('| / \\\n')
+
+    print('- ' * tamanho_da_palavra)
+
+    while(not enforcou and not acertou):
+        chute = input('Digite uma letra: ')
+        chute = chute.strip().upper()
+
+        if(chute in palavra_secreta):
+            index = 0 
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[index] = letra
+                index = index + 1
+        else:
+            erros += 1  
+
+        enforcou = erros == 6
+        acertou = '-' not in letras_acertadas
+        print(str(letras_acertadas))
+
+    if(acertou):
+        print('\nParabéns! Você acertou!\n')
+    else:
+        print('\nGame Over! Você errou!\n')
+
 if(__name__ == '__main__'):
     jogo()
